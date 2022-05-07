0099f178a8c926c59c3adbc51aeaddcb0e0530fe
Brandon Chavez <bchaveznoguera@gmail.com>
2022-05-07 13:06:27 -0500

    Firebase

--- src/app/services/info-pages.service.ts
+++ src/app/services/info-pages.service.ts
@@ -9,14 +9,28 @@ export class InfoPagesService {
 
   info: InfoPages = {};
   loaded: boolean = false;
+  team: any[] = [];
 
   constructor(private http: HttpClient) {
+    this.loadInfo();
+    this.loadTeam();
+  }
+
+  private loadInfo() {
     console.log('Servicio de infoPages listo!');
 
     this.http.get('assets/data/data-pages.json').subscribe( (resp: InfoPages) => {
       this.loaded = true;
       this.info = resp;
-      console.log(resp);
+    });
+  }
+
+  private loadTeam() {
+    console.log('Servicio de Firebase listo!');
+
+    this.http.get('https://angular-portafolio-71333-default-rtdb.firebaseio.com/equipo.json').subscribe( (resp: any) => {
+      this.loaded = true;
+      this.team = resp;
     });
   }
 }
